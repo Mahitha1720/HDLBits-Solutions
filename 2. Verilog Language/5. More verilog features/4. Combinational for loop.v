f5d04933204eb900213ff7453fbd4b96df4a53d5
Mahitha <mahitha1727@gmail.com>
2025-08-04 19:18:45 +0530

    Create 4. Combinational for loop.v

---
+++ 2. Verilog Language/5. More verilog features/4. Combinational for loop.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    always @(*) begin
+        integer i;
+        for(i=0;i<100;i=i+1)
+                out[99-i]=in[i];
+    end
+
+endmodule
